f1908104a784154b1d2f05f11e07f394d189b18b
Thunyatorn Kongmoung <Thunyatorn1125@gmail.com>
2023-12-22 16:23:21 +0700

    lab11

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+int main(){
+    int x=0;
+    srand(time(0));
+    string grade[9]={"A", "B+", "B", "C+","C", "D+", "D", "F","W"};
+    cout << "Press Enter 3 times to reveal your future.\n";
+    while(x < 3 ){
+        cin.get() ;
+        x++;
+    }
+    cout << "You will get "<<grade[rand()%9]<<" in this 261102.";
+    return 0;
+}
+
